0356957bd146fdba970b7f828e6e3a59d9b15162
danilovalerio <danilo_vs@hotmail.com>
2018-11-06 16:44:28 -0200

    Mais exemplo do uso das funções com funções de parametros

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,11 +15,22 @@ public class Programa {
 		List<Integer> list = Arrays.asList(3,4,5,10,7);
 		
 		//Cria uma stream<Tipo> a partir da lista
-		Stream<Integer> st1 = list.stream(); 
+		Stream<Integer> st1 = list.stream().map(x -> x *10);//multiplica o elemento por 10 
 		
 		//Imprime uma array com o stream convertido em vetor através do toArray()
 		System.out.println(Arrays.toString(st1.toArray()));
-
+		
+		//Cria uma stream de Strings
+		Stream<String> st2 = Stream.of("Ana", "Maria", "João", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		//Stream de Iteração
+		Stream<Integer> st3 = Stream.iterate(0,  x-> x+2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));//limit(y) limita até quantas vezes irá repetir
+		
+		//Sequencia de fibonacci
+		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]);
+		System.out.println(Arrays.toString(st4.limit(15).toArray()));
 	}
 
 }
